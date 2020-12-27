ae32b38c55ad664ae2b9b8ddde22ce1cbb794bbc
redpeacock78 <scarletpeacock78@gmail.com>
2020-12-28 02:54:00 +0900

    Change content: install_with_stow.sh

--- install_with_stow.sh
+++ install_with_stow.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 mkdir ~/stow
-declare stow="~/stow"
+declare stow=~/stow
 declare version=$(libexec/lab -v | head -n1 | tr ' ' -)
 
 # Unstow older versions of Bats.
 shopt -s nullglob
 
-declare old_lab=$(find "$stow" -type d -name lab-\*)
+declare old_lab=$(find "${stow}" -type d -name lab-\*)
 
 for package in "${old_lab[@]}"; do
     stow --dir="${stow}" --delete $(basename "${package}")
