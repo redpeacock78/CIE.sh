852637c22f40083bc93ae889207b6783715cc01c
redpeacock78 <scarletpeacock78@gmail.com>
2020-12-28 02:51:26 +0900

    Add content: install_with_stow.sh

---
+++ install_with_stow.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir ~/stow
+declare stow="~/stow"
+declare version=$(libexec/lab -v | head -n1 | tr ' ' -)
+
+# Unstow older versions of Bats.
+shopt -s nullglob
+
+declare old_lab=$(find "$stow" -type d -name lab-\*)
+
+for package in "${old_lab[@]}"; do
+    stow --dir="${stow}" --delete $(basename "${package}")
+done
+
+./install.sh "${stow}/${version}"
+stow --dir="${stow}" --restow "${version}"
